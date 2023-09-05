d9af65e53e79b61c43c40950d17191f3eb8e2314
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2023-09-05 20:02:54 -0300

    connect to a webapp using webview

--- app/src/main/java/com/dreyer/coffeemasters/pages/InfoPage.kt
+++ app/src/main/java/com/dreyer/coffeemasters/pages/InfoPage.kt
@@ -1,10 +1,34 @@
 package com.dreyer.coffeemasters.pages
 
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 
-@Preview
+@Composable
+fun WebView() {
+    // Declare a string that contains a url
+    val url = "https://firtman.github.io/coffeemasters/webapp"
+
+    // Adding a WebView inside AndroidView
+    // with layout as full screen
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    }, update = {
+        it.loadUrl(url)
+    })
+}
+
 @Composable
 fun InfoPage() {
-    
+    WebView()
 }
